628f242a00d105dd4695d4fb101f6636566ae8e8
Stefan Martin <stefanm1010@gmail.com>
2020-01-09 11:51:38 -0500

    Added gaussian white noise option

--- scripts/plot_sim_displacement.m
+++ scripts/plot_sim_displacement.m
@@ -1,7 +1,13 @@
 f_samp = 10000;
 amplitude_wavelengths = 100;
+SNR=10;
+Noisy=1;
 [out,in] = smi_signal_simulation(f_samp,1,f_samp,amplitude_wavelengths,0,6,3);
-
+if Noisy
+    out = awgn(out,SNR)
+end
+csvwrite('C:\Users\TEMP.NEXUS\Desktop\out.csv',out);
+csvwrite('C:\Users\TEMP.NEXUS\Desktop\in.csv',in)
 ax1 = subplot(2,1,1);
 plot(ax1,out)
 title(ax1, "Input and Resulting SMI Signal")
