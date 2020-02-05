52d31815f72a9e50dbebd66bf1cfab17f611f1cb
Stefan <stefanm1010@gmail.com>
2020-02-05 18:43:20 -0500

    New fringe test

--- scripts/proto_fringe_detector.py
+++ scripts/proto_fringe_detector.py
@@ -2,7 +2,7 @@ from scripts import step_detect
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import signal
-import cv2
+#import cv2
 
 def find_inflections_above_under(signal, low_threshold, high_threshold, group_threshold = 50):
 
@@ -25,28 +25,36 @@ def find_inflections_above_under(signal, low_threshold, high_threshold, group_th
 
     return above_infl, below_infl
 
-def jump_magnitude_threshold(candidate_fringes,smi_signal,jump_threshold=0.08,window_size=60):
+def jump_magnitude_threshold(candidate_fringes,smi_signal,jump_threshold=1.2,window_scale=20):
     fringes=[]
+    window_size=int(window_scale*np.ceil(sampling_rate/100000))
     center=window_size
     for i in candidate_fringes:
-        window=smi_signal[i-window_size:i+window_size] #make sampling rate-based
+        window=smi_signal[i-window_size:i+window_size]
         extrema = np.sign(np.diff(window))
         extrema[2 * window_size-2] = 1
         for j,k in enumerate (extrema):
             if extrema[j] == 0:
-                extrema[j]=extrema[j-1]
-        extrema=np.diff(extrema).nonzero()[0]+1
-        a=extrema[extrema < center].max() #first peak above
-        b=extrema[extrema >center].min()  # first peak below
-        plt.plot(window, '-rD', markevery=[a,b])
-        plt.show()
-        #print(abs(window[a]-window[b]))
-        if abs(window[a]-window[b])>jump_threshold:
+
+                extrema[j]=extrema[j-1] #to avoid treating zero as a sign change we hold the prior value of the derivative
+        extrema=np.diff(extrema).nonzero()[0]+1 #find all the derivative sign changes, these are all local extrema
+        peaks_below=extrema[extrema < center]
+        peaks_above=extrema[extrema >center]
+        if len(peaks_above)==0 or len(peaks_below)==0: #not a real discontinuity since no extrema above or below
+            continue
+        a=peaks_below.max() #index of first peak below
+        b=peaks_above.min()  # index of first peak above
+        #plt.plot(window, '-rD', markevery=[a,b]) #show each fringe
+        #plt.show()
+        if abs(window[a]-window[b])>jump_threshold*np.std(smi_signal):
             fringes.append(i)
     return fringes
+
 std_threshold = 1.5
 sampling_rate = 300000
-process_time_bounds = [8.57, 8.61]
+wavelength=639
+process_time_bounds = [5, 9.1]
+
 full_signal=np.load('/home/stefan/smi_data/target_on.npy').flatten()/(2 ** 12)
 reduced_signal = full_signal[int(process_time_bounds[0]* sampling_rate):int(process_time_bounds[1] * sampling_rate)]
 times = np.array(list(range(len(reduced_signal))))/sampling_rate + process_time_bounds[0]
@@ -71,7 +79,7 @@ possible_fringe_indicies= np.argwhere(np.abs(filtered_gradient - mean_grad) > st
 plot, axs = plt.subplots(nrows=3, ncols=1)
 max_sig = np.max(filtered_signal)
 min_sig = np.min(filtered_signal)
-axs[0].plot(times, filtered_signal, linewidth = 0.5, color='r')
+axs[0].plot(times, filtered_signal, linewidth = 0.2, color='r')
 for fr in pos_fringes:
     axs[0].plot([times[fr], times[fr]], [min_sig, max_sig], color='orange')
 for fr in neg_fringes:
@@ -94,8 +102,8 @@ axs[1].set_ylabel('Multiscale Edges')
 #axs[1].set_yscale('log')
 axs[2].set_xlabel('time (s)')
 
-
-axs[0].set_title('Net travel: {} fringes'.format(len(pos_fringes) - len(neg_fringes)))
+net_fringes=len(pos_fringes) - len(neg_fringes)
+axs[0].set_title('Net travel: {} um'.format(wavelength*net_fringes/1000))
 
 plot.show()
 
