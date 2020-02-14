f76bd8be3ff174c0c14605ca230a36441018b438
Stefan <stefanm1010@gmail.com>
2020-02-14 13:19:01 -0500

    Naming and metadata

--- scripts/data_collection/bidirectional_collect.py
+++ scripts/data_collection/bidirectional_collect.py
@@ -26,7 +26,7 @@ def main():
         if pidevice.HasqVER():
             print('version info:\n{}'.format(pidevice.qVER().strip()))
 
-        print('initialize connected stages...')
+        print('initialize stage...')
         pitools.startup(pidevice, stages=STAGES, refmodes=REFMODES)
 
         rangemin = pidevice.qTMN() # min position
@@ -40,36 +40,45 @@ def main():
               'current max velocity: {},\n'
               'current max accelereation: {}'.format(rangemin, rangemax, curpos, curvel, curacc))
 
-
+        velocity=0.005 #mm/s
+        accel=0.1 #mm/s^2
+        OD=0.4 #optical density
+        distance_moved=0.01 #mm, 1 direction
+        file_index=1 #file index
+        sample_rate=400000 #DAQ sample rate
+        laser_current=35
+        metadata=np.array([velocity,accel,OD,distance_moved,file_index,sample_rate,laser_current]) #metadata for move
+        if distance_moved>rangemax['1']:
+            return -1
         pidevice.VEL(pidevice.axes[0],0.5) # set velocity to 0.3 mm/s
-        pidevice.ACC(pidevice.axes[0], 0.5) # set acceleration to 0.1 mm/s^2
-        pidevice.MOV(pidevice.axes[0], 2)
+        pidevice.ACC(pidevice.axes[0], 0.5) # set acceleration
+        pidevice.MOV(pidevice.axes[0], 1)
         while pidevice.IsMoving(axes=pidevice.axes[0])['1']:
             pass
         print('starting recording')
 
-        res = comedi_async.sample_time_async([0],[1], 60000, 20)
+        res = comedi_async.sample_time_async([0],[1], sample_rate, 6)
 
-        pidevice.VEL(pidevice.axes[0], 0.005)  # set velocity to 0.3 mm/s
-        pidevice.ACC(pidevice.axes[0], 0.01)
+        pidevice.VEL(pidevice.axes[0], velocity)  # set velocity
+        pidevice.ACC(pidevice.axes[0], accel)   # set accel
 
-        time.sleep(5)
+        time.sleep(0.5) #wait 1 s before moving
         while pidevice.IsMoving(axes=pidevice.axes[0])['1']:
             pass
         print('moving out')
-        pidevice.MOV(pidevice.axes[0],1.975)
+        pidevice.MOV(pidevice.axes[0],1+distance_moved)
         while pidevice.IsMoving(axes=pidevice.axes[0])['1']:
             pass
         print('moving back')
-        pidevice.MOV(pidevice.axes[0], 2)
+        pidevice.MOV(pidevice.axes[0],1)
         while pidevice.IsMoving(axes=pidevice.axes[0])['1']:
             pass
-        time.sleep(5)
-        output = '/home/trevor/smi_data/OD_sweep/24-01-20_3'
+        time.sleep(0.5)
+        output = '/home/stefan/smi_data/14-02-2020'
         os.makedirs(output, exist_ok=True)
-        np.save(os.path.join(output, '5mum_s_solid_OD03_16.npy'), res.get())
-        print('recording done')
-        print("")
+        np.save(os.path.join(output,str(distance_moved) +'mm_'+str(file_index)+'.npy'), res.get())
+        np.save(os.path.join(output,str(distance_moved) +'mm_'+str(file_index)+'_meta.npy'),metadata)
+        print('done')
 
 if __name__ == '__main__':
     main()
